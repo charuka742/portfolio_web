d011da0bcf01a999f1013abd0441728413efc232
Disanayaka AAC <charair742@gmail.com>
2023-05-14 17:12:15 +0530

    created components + added simply routed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+import Content from './Components/Content';
+// import {Landing} from './Components/Landing';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <h1 className='font-semibold text-2xl'>Hello!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Landing />}> */}
+
+        <Route index element={<Navbar />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/content" element={<Content />} />
+
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
